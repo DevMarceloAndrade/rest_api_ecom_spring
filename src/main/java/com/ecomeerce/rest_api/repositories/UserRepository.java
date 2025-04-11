fffcd2a7c360e2e87f13f5ef16c0be3d7bb743c4
Marcelo Leal <149432616+Ellucidator@users.noreply.github.com>
2025-04-11 12:02:47 -0300

    feat: implementação do metodo de validação de password

--- src/main/java/com/ecomeerce/rest_api/repositories/UserRepository.java
+++ src/main/java/com/ecomeerce/rest_api/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package com.ecomeerce.rest_api.repositories;
 import com.ecomeerce.rest_api.models.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends DataBaseRepository<User> {
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/ecomeerce/rest_api/services/UserService.java
+++ src/main/java/com/ecomeerce/rest_api/services/UserService.java
@@ -1,4 +1,6 @@
 package com.ecomeerce.rest_api.services;
+import com.ecomeerce.rest_api.exception.EntityDoesNotExist;
+import com.ecomeerce.rest_api.exception.InvalidEntityException;
 import com.ecomeerce.rest_api.models.User;
 import com.ecomeerce.rest_api.repositories.UserRepository;
 import jakarta.validation.Validator;
@@ -9,11 +11,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService extends BaseService<User>{
 
+    private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository dataBaseRepository, Validator validator, PasswordEncoder passwordEncoder) {
         super(dataBaseRepository, validator);
+        this.userRepository = dataBaseRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -23,4 +27,13 @@ public class UserService extends BaseService<User>{
         return super.create(entity);
     }
 
+    public User passwordMatch(String email,String pass){
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(EntityDoesNotExist::new);
+        if (!passwordEncoder.matches(pass, user.getPassword())){
+            throw new InvalidEntityException("");
+        }
+        
+        return user;
+    }
 }
